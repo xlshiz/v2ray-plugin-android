b5e38aed2d17c861c2d5412e87b3b30631555660
Teddysun <i@teddysun.com>
2020-08-29 12:19:50 +0900

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(minSdk)
         targetSdkVersion(29)
-        versionCode = 1040000
-        versionName = "1.4.0"
+        versionCode = 1040100
+        versionName = "1.4.1"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
