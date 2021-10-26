c490ec5c1ee93dca7706e3239f13f88880a406be
Teddysun <i@teddysun.com>
2021-10-26 16:20:09 +0900

    Bump version v4.43.0
    Signed-off-by: Teddysun <i@teddysun.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 4042020
-        versionName = "4.42.2"
+        versionCode = 4043000
+        versionName = "4.43.0"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
