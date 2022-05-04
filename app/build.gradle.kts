fd8d15c15e2937601d6ca522cf98ebf1b20a07d2
Teddysun <i@teddysun.com>
2022-05-04 10:11:49 +0900

    Bump version v5.0.6
    Signed-off-by: Teddysun <i@teddysun.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 5000500
-        versionName = "5.0.5"
+        versionCode = 5000600
+        versionName = "5.0.6"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
